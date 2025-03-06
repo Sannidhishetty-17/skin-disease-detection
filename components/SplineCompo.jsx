8db4e9d85082882d466d907ca2299a4f0309d7a2
Sannidhi Shetty <sannidhii.1609@gmail.com>
2025-03-06 19:55:21 +0530

    Add files via upload
    added spline 3d in about section

---
+++ components/SplineCompo.jsx
@@ -0,0 +1,8 @@
+import Spline from '@splinetool/react-spline';
+
+export default function SplineCompo() {
+  return (
+    <Spline scene="https://prod.spline.design/5IQzf0zBAD7vFeDp/scene.splinecode" />
+  );
+}
+
