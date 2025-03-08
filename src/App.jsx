28ae5bfb86d9921131e4a2274046088d450f8446
Sannidhi Shetty <sannidhii.1609@gmail.com>
2025-03-08 10:08:16 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './App.css';
+import Home from './components/Home'
+import Navbar from './components/Navbar'
+import About from './components/About'
+
+
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <Home />
+      <About />
+  
+    </div>
+  )
+}
+
+export default App
