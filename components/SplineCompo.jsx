6063bf2ae67b39e2d17167a80d956dac29eb9709
Sannidhi Shetty <sannidhii.1609@gmail.com>
2025-03-08 10:05:09 +0530

    Delete components directory

--- components/SplineCompo.jsx
+++
@@ -1,8 +0,0 @@
-import Spline from '@splinetool/react-spline';
-
-export default function SplineCompo() {
-  return (
-    <Spline scene="https://prod.spline.design/5IQzf0zBAD7vFeDp/scene.splinecode" />
-  );
-}
-
